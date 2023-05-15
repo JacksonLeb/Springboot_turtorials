36cf89a297ddefd7e37255ab4c42d60aadae3c07
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2023-05-15 14:28:18 -0400

    SpringBoot download project

---
+++ turtorial/src/test/java/com/jacksonleb/turtorial/TurtorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonleb.turtorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
