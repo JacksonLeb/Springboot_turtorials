348de2e2fad0cb3f40397373589e17e945427457
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2023-05-18 17:13:43 -0400

    created RESTful api and default responses for root endpoint

--- turtorial/src/main/java/com/jacksonleb/Main.java
+++ turtorial/src/main/java/com/jacksonleb/Main.java
@@ -2,10 +2,28 @@ package com.jacksonleb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse(
+                "hello",
+                List.of("Java", "Javascript", "Python"),
+                new Person("Jackson", 28, 500.12));
+    }
+
+    record Person(String name, int age, double savings) {
+    }
+
+    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {
     }
 }
