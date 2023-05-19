e9840c720056251644e69e307e3320ed373e1c9c
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2023-05-19 15:33:36 -0400

    created postgresql container with Docker, finished CRUD implementation

--- turtorial/src/main/java/com/jacksonleb/Main.java
+++ turtorial/src/main/java/com/jacksonleb/Main.java
@@ -2,28 +2,53 @@ package com.jacksonleb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse(
-                "hello",
-                List.of("Java", "Javascript", "Python"),
-                new Person("Jackson", 28, 500.12));
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
-    record Person(String name, int age, double savings) {
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age) {
+
     }
 
-    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
     }
 }
